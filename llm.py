3121bd1b509f15332ee0a56d57c7ac1b8343df1b
Dongook Son <60206749+donny-son@users.noreply.github.com>
2024-10-10 01:24:12 +0900

    init: 차라투와 통신

--- llm.py
+++ llm.py
@@ -1,19 +1,20 @@
+import json
 import os
-import ell
 from typing import List, Optional
+
+import ell
+import tqdm
+from dotenv import load_dotenv
 from pydantic import BaseModel
 from sqlalchemy import create_engine
-from sqlmodel import Field, SQLModel, Session, select
-from dotenv import load_dotenv
-import tqdm
-
+from sqlmodel import Field, Session, SQLModel, select
 
 load_dotenv()
 
 DB_URL = os.getenv("DB_URL")
 engine = create_engine(DB_URL)
 
-ell.init(store="./log")
+# ell.init(store="./log")
 
 
 class SpecificQuestion(BaseModel):
@@ -28,418 +29,135 @@ class ListOfQuestions(BaseModel):
     questions: List[SpecificQuestion]
 
 
-class Question(SQLModel, table=True):
-    __tablename__ = "questions"
-    id: Optional[int] = Field(
-        None,
-        description="Primary key for the questions table, auto-incremented.",
-        primary_key=True,
-    )
-    diagnosis_code_id: Optional[int] = Field(
-        None, description="Foreign key reference to diagnosis_codes table."
-    )
-    category: Optional[str] = Field(None, description="Category of the question.")
-    keyword: Optional[str] = Field(None, description="Keyword for the question.")
-    question: Optional[str] = Field(None, description="The actual question.")
-    is_valid: Optional[bool] = Field(None, description="Validity flag of the question.")
-    reason: Optional[str] = Field(
-        None, description="Reason for the question being invalid (if applicable)."
-    )
+class Question(BaseModel):
+    question_kor: str
+    groupCodeId: str
+    categoryId: str
+    isAppropriate: bool = Field(default=False)
+    isAIGenerated: bool = Field(default=False)
 
 
-class DiagnosisCode(SQLModel, table=True):
-    __tablename__ = "diagnosis_codes"
-    id: int = Field(default=None, primary_key=True)
-    usage_flag: Optional[str] = None
-    usage_level: Optional[int] = None
-    group_code: Optional[str] = None
-    code_level_1: Optional[str] = None
-    dx_kor_level_1: Optional[str] = None
-    dx_en_level_1: Optional[str] = None
-    code_level_2: Optional[str] = None
-    dx_kor_level_2: Optional[str] = None
-    dx_en_level_2: Optional[str] = None
-    code_level_3: Optional[str] = None
-    dx_kor_level_3: Optional[str] = None
-    dx_en_level_3: Optional[str] = None
-    code_level_4: Optional[str] = None
-    dx_kor_level_4: Optional[str] = None
-    dx_en_level_4: Optional[str] = None
-    code_level_5: Optional[str] = None
-    dx_kor_level_5: Optional[str] = None
-    dx_en_level_5: Optional[str] = None
-    code_level_6: Optional[str] = None
-    dx_kor_level_6: Optional[str] = None
-    dx_en_level_6: Optional[str] = None
+class Category(BaseModel):
+    id: str = Field(default=None)
+    name_kor: str = Field(default=None)
+    name_eng: str = Field(default=None)
 
 
-prompt = """
+class GroupCode(BaseModel):
+    id: str = Field(default=None)
+    code: str = Field(default=None)
+    name: str = Field(default=None)
+    kor_name: str = Field(default=None)
+    eng_name: str = Field(default=None)
+    department: str = Field(default=None)
+
+
+prompt = f"""
 I want to create a disease specific patient survey question for history and physical examination.
 
-The disease database I have has the following table.
+The disease data I have has the following schema.
 
-```sql
-CREATE TABLE "public"."diagnosis_codes" (
-    "id" int4 NOT NULL DEFAULT nextval('diagnosis_codes_id_seq'::regclass),
-    "usage_flag" varchar,
-    "usage_level" int4,
-    "group_code" varchar,
-    "code_level_1" varchar,
-    "dx_kor_level_1" varchar,
-    "dx_en_level_1" varchar,
-    "code_level_2" varchar,
-    "dx_kor_level_2" varchar,
-    "dx_en_level_2" varchar,
-    "code_level_3" varchar,
-    "dx_kor_level_3" varchar,
-    "dx_en_level_3" varchar,
-    "code_level_4" varchar,
-    "dx_kor_level_4" varchar,
-    "dx_en_level_4" varchar,
-    "code_level_5" varchar,
-    "dx_kor_level_5" varchar,
-    "dx_en_level_5" varchar,
-    "code_level_6" varchar,
-    "dx_kor_level_6" varchar,
-    "dx_en_level_6" varchar,
-    PRIMARY KEY ("id")
-);
+```json
+{str(GroupCode.model_json_schema())}
 ```
 
-Here is an example of the following query.
+Here is an example of the data.
 
-```sql
-SELECT *
-from "public".diagnosis_codes
-where dx_kor_level_3 like '%암%';
-```
 
 ```json
 [
-  {
-    "id": 1697,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.0-D01.4",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.1",
-    "dx_kor_level_4": "직장구불결장접합부의 제자리암종",
-    "dx_en_level_4": "Carcinoma in situ of rectosigmoid junction",
-    "code_level_5": "",
-    "dx_kor_level_5": "",
-    "dx_en_level_5": "",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  },
-  {
-    "id": 1698,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.0-D01.4",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.2",
-    "dx_kor_level_4": "직장의 제자리암종 ",
-    "dx_en_level_4": "Carcinoma in situ of rectum",
-    "code_level_5": "",
-    "dx_kor_level_5": "",
-    "dx_en_level_5": "",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  },
-  {
-    "id": 1699,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.0-D01.4",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.3",
-    "dx_kor_level_4": "항문 및 항문관의 제자리암종 ",
-    "dx_en_level_4": "Carcinoma in situ of anus and anal canal",
-    "code_level_5": "",
-    "dx_kor_level_5": "",
-    "dx_en_level_5": "",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  },
-  {
-    "id": 1700,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.0-D01.4",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.4",
-    "dx_kor_level_4": "기타 및 상세불명 부분의 장의 제자리암종",
-    "dx_en_level_4": "Carcinoma in situ of other and unspecified parts of intestine",
-    "code_level_5": "",
-    "dx_kor_level_5": "",
-    "dx_en_level_5": "",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  },
-  {
-    "id": 1701,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.5",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.5",
-    "dx_kor_level_4": "간, 담낭 및 담관의 제자리암종 ",
-    "dx_en_level_4": "Carcinoma in situ of liver, gallbladder and bile ducts",
-    "code_level_5": "",
-    "dx_kor_level_5": "",
-    "dx_en_level_5": "",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  },
-  {
-    "id": 1702,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.5",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.5",
-    "dx_kor_level_4": "간, 담낭 및 담관의 제자리암종 ",
-    "dx_en_level_4": "Carcinoma in situ of liver, gallbladder and bile ducts",
-    "code_level_5": "D01.50",
-    "dx_kor_level_5": "간의 제자리암종",
-    "dx_en_level_5": "Carcinoma in situ of liver",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  },
-  {
-    "id": 1703,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.5",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.5",
-    "dx_kor_level_4": "간, 담낭 및 담관의 제자리암종 ",
-    "dx_en_level_4": "Carcinoma in situ of liver, gallbladder and bile ducts",
-    "code_level_5": "D01.51",
-    "dx_kor_level_5": "담낭의 제자리암종",
-    "dx_en_level_5": "Carcinoma in situ of gallbladder",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  },
-  {
-    "id": 1704,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.5",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.5",
-    "dx_kor_level_4": "간, 담낭 및 담관의 제자리암종 ",
-    "dx_en_level_4": "Carcinoma in situ of liver, gallbladder and bile ducts",
-    "code_level_5": "D01.52",
-    "dx_kor_level_5": "담관의 제자리암종",
-    "dx_en_level_5": "Carcinoma in situ of bile ducts",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  },
-  {
-    "id": 1705,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.5",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.5",
-    "dx_kor_level_4": "간, 담낭 및 담관의 제자리암종 ",
-    "dx_en_level_4": "Carcinoma in situ of liver, gallbladder and bile ducts",
-    "code_level_5": "D01.53",
-    "dx_kor_level_5": "파터팽대의 제자리암종",
-    "dx_en_level_5": "Carcinoma in situ of ampulla of Vater",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  },
-  {
-    "id": 1706,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.5",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.5",
-    "dx_kor_level_4": "간, 담낭 및 담관의 제자리암종 ",
-    "dx_en_level_4": "Carcinoma in situ of liver, gallbladder and bile ducts",
-    "code_level_5": "D01.59",
-    "dx_kor_level_5": "상세불명의 간담도계의 제자리암종",
-    "dx_en_level_5": "Carcinoma in situ of hepatobiliary system, unspecified",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-  }
+    {{
+        "id": "2735303c-fbe5-4f30-8761-39b4b752b34b",
+        "code": "A19",
+        "name": "A19",
+        "kor_name": "좁쌀결핵",
+        "eng_name": "Miliary tuberculosis",
+        "department": "UNASSIGNED"
+    }},
+    {{
+        "id": "9e7aa9cf-78a0-40e8-981d-ebe8427d5218",
+        "code": "A18",
+        "name": "A18",
+        "kor_name": "기타 기관의 결핵",
+        "eng_name": "Tuberculosis of other organs",
+        "department": "UNASSIGNED"
+    }},
+    {{
+        "id": "ba30d79b-0d7c-4395-ab84-b082ede9862a",
+        "code": "A18.4",
+        "name": "A18.4",
+        "kor_name": "피부 및 피하조직의 결핵",
+        "eng_name": "Tuberculosis of skin and subcutaneous tissue",
+        "department": "UNASSIGNED"
+    }}
 ]
 ```
 
-You will be receiving a row from the `diagnosis_codes` table. You need to create a list of questions that should be asked to the patient based on the diagnosis. The questions should be in Korean. Here is an example of the input and the output. The `category` can be the following values: "Chief complaint", "Medical history", "Family history", "Allergy", "Surgical history", "Lifestyle", "Medication", and "Review of system".
+You will be receiving a data from the `groupCode`. You need to create a list of questions that should be asked to the patient based on the diagnosis. The questions should be in Korean. Here is an example of the input and the output. The `category` can be the following values: "Chief complaints", "Past medical history", "Family history", "Allergy", "Past surgical history", "Lifestyle", "Medication", and "Review of system".
 
 ```input
-{
-    "id": 1698,
-    "usage_flag": "1",
-    "usage_level": 4,
-    "group_code": "D01.0-D01.4",
-    "code_level_1": "C00-D48",
-    "dx_kor_level_1": "신생물(C00-D48)",
-    "dx_en_level_1": "Neoplasms(C00-D48)",
-    "code_level_2": "D00-D09",
-    "dx_kor_level_2": "제자리신생물(D00-D09)",
-    "dx_en_level_2": "In situ neoplasms(D00-D09)",
-    "code_level_3": "D01",
-    "dx_kor_level_3": "기타 및 상세불명의 소화기관의 제자리암종",
-    "dx_en_level_3": "Carcinoma in situ of other and unspecified digestive organs",
-    "code_level_4": "D01.2",
-    "dx_kor_level_4": "직장의 제자리암종 ",
-    "dx_en_level_4": "Carcinoma in situ of rectum",
-    "code_level_5": "",
-    "dx_kor_level_5": "",
-    "dx_en_level_5": "",
-    "code_level_6": "",
-    "dx_kor_level_6": "",
-    "dx_en_level_6": ""
-}
+{{
+    "id": "e72f8df4-7134-4f2c-921c-51b37edfd247",
+    "code": "C19-C20",
+    "name": "C19-C20",
+    "kor_name": "직장구불결장접합부의 악성 신생물, 직장의 악성 신생물",
+    "eng_name": "Malignant neoplasm of rectosigmoid junction, Malignant neoplasm of rectum",
+    "department": "UNASSIGNED"
+}}
 ```
 
 ```output
 [
-    {
-        "category": "Medical history",
+    {{
+        "category": "Past medical history",
         "keyword": "colonoscopy",
         "question": "대장내시경 검사를 해보셨나요?"
-    },
-    {
-        "category": "Medical history",
+    }},
+    {{
+        "category": "Past medical history",
         "keyword": "colonoscopy",
         "question": "대장내시경 검사를 해보셨나요?"
-    },
-    {
+    }},
+    {{
         "category": "Review of system",
         "keyword": "Stool caliber change",
         "question": "대변 굵기 변화"
-    },
-    {
+    }},
+    {{
         "category": "Review of system",
         "keyword": "Anal pain",
         "question": "항문 통증"
-    },
-    {
+    }},
+    {{
         "category": "Review of system",
         "keyword": "Pruritis ani",
         "question": "항문 가려움"
-    },
-    {
+    }},
+    {{
         "category": "Review of system",
         "keyword": "Tenesmus",
         "question": "후중감(항문이나 직장에 무언가 남아있는 것 같은 불편한 느낌)"
-    },
-    {
+    }},
+    {{
         "category": "Review of system",
         "keyword": "Discharge(soiling)",
         "question": "대변이 묻어나오는 증상"
-    },
-    {
+    }},
+    {{
         "category": "Review of system",
         "keyword": "Prolapsed anal mass",
         "question": "항문 종괴 돌출"
-    },
-    {
+    }},
+    {{
         "category": "Review of system",
         "keyword": "Urinary incontinence",
         "question": "요실금"
-    },
-    {
+    }},
+    {{
         "category": "Review of system",
         "keyword": "Fecal incontinence",
         "question": "변실금"
-    }
+    }}
 ]
 ```
 
@@ -518,26 +236,40 @@ def generate_question(diagnosis_code_str: str) -> ListOfQuestions:
     return [ell.system(prompt), ell.user(diagnosis_code_str)]
 
 
-if __name__ == "__main__":
-    with Session(engine) as session:
-        statement = (
-            select(DiagnosisCode)
-            .where(DiagnosisCode.code_level_4 == "")
-            .where(
-                DiagnosisCode.id.not_in(select(Question.diagnosis_code_id).distinct())
-            )
-        )
-        diagnosis = session.exec(statement).all()
+def get_categories() -> List[Category]:
+    import requests
 
-    with Session(engine) as session:
-        for d in tqdm.tqdm(diagnosis):
-            questions = generate_question(d.model_dump_json())
-            for sq in questions.parsed.questions:
-                q = Question(
-                    diagnosis_code_id=d.id,
-                    category=sq.category,
-                    keyword=sq.keyword,
-                    question=sq.question,
-                )
-                session.add(q)
-            session.commit()
+    url = "https://vexam.rnd.zarathu.com/api/rest/v1/question/category"
+
+    payload = {}
+    headers = {"x-api-key": "zCVwmOBR1pMY/Iva+xZndQr2PQ+RLSm+V8t221nGjMw="}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    data = json.loads(response.text)
+    output = []
+    for d in data["data"]:
+        output.append(Category(**d))
+    return output
+
+
+def get_group_codes() -> List[GroupCode]:
+    import requests
+
+    url = "https://vexam.rnd.zarathu.com/api/rest/v1/groupCode"
+
+    payload = {}
+    headers = {"x-api-key": "zCVwmOBR1pMY/Iva+xZndQr2PQ+RLSm+V8t221nGjMw="}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    data = json.loads(response.text)
+    output = []
+    for d in data["data"]:
+        output.append(GroupCode(**d))
+    return output
+
+
+if __name__ == "__main__":
+    print(get_group_codes())
+    print(get_categories())
